363373211b40b45ea046bda84a16f57cd635c14b
sivakarthikeyan26 <50573680+sivakarthikeyan26@users.noreply.github.com>
2022-08-13 19:53:15 +0530

    Program to test the regex.java is implemented

--- Junit.java
+++ Junit.java
@@ -9,22 +9,30 @@ public class Junit {
 		
 		@Mock
 		private Regex rg = new Regex();
-		
+		/**
+		 * testing first name
+		 */
 		@Test
 		public void fname() {
 			assertTrue(Regex.firstname("Siva"));
 		}
-
+		/**
+		 * testing last name
+		 */
 		@Test
 		public void lname() {
 			assertTrue(Regex.lastname("Karthi"));
 		}
-		
+		/**
+		 * testing password
+		 */
 		@Test
 		public void pass() {
 			assertTrue(Regex.password("asdfghjkS$"));
 		}
-		
+		/**
+		 * testing email
+		 */
 		@Test
 		public void email() {
 			assertTrue(Regex.email("karthikeyan26@gmail.com"));
